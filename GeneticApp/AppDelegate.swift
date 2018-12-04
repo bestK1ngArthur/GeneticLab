f5e0b9ac13c7ce3957bf34b946c552baa80cd667
Artem Belkov <a.belkov@corp.mail.ru>
2018-12-04 12:29:39 +0300

    Initial Commit

---
+++ GeneticApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GeneticApp
+//
+//  Created by a.belkov on 04/12/2018.
+//  Copyright © 2018 bestK1ng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GeneticApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GeneticApp
+//
+//  Created by a.belkov on 04/12/2018.
+//  Copyright © 2018 bestK1ng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
